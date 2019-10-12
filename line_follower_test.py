e695ef1c4111b70681df4c71aeddb1e8140c5ccd
Juuso Terävä <juuso.terava@relex.fi>
2019-10-12 11:42:28 +0300

    Fixes

--- line_follower_test.py
+++ line_follower_test.py
@@ -12,8 +12,6 @@ class LineFollowerTest:
     FORWARD_SPEED = 50
     SEARCH_SPEED = 25
     STEPS_PER_SECOND = 1 / TIME_STEP
-    COLOR_BLUE = 2
-    COLOR_RED = 5
     LEFT = -1
     RIGHT = 1
     VEERING_THRESHOLD = 10000  # Veering off
@@ -41,24 +39,23 @@ class LineFollowerTest:
         global last_search_direction
         last_brightness = 1000
         brightness = last_brightness
-        self.steering_drive.on_for_seconds(0, SpeedPercent(speed), TIME_STEP * STEPS_PER_SECOND * 10, False, False)
+        self.steering_drive.on_for_seconds(0, SpeedPercent(speed), LineFollowerTest.TIME_STEP * LineFollowerTest.STEPS_PER_SECOND * 10, False, False)
         while on_track():
             print(last_brightness)
             effective_direction = direction
-            if last_brightness < brightness - VEERING_THRESHOLD:
+            if last_brightness < brightness - LineFollowerTest.VEERING_THRESHOLD:
                 effective_direction = last_search_direction * 10
                 print('Veering ' + direction_name(last_search_direction))
-            #self.steering_drive.on_for_seconds(effective_direction, SpeedPercent(-speed), TIME_STEP, False)
+            #self.steering_drive.on_for_seconds(effective_direction, SpeedPercent(-speed), LineFollowerTest.TIME_STEP, False)
             brightness = last_brightness
-            #TODO: Check if we got new instructions from websocket
 
         self.steering_drive.off(None, False)
 
     def move_until_on_track(self, direction, speed, max_steps):
         i = 0
-        self.steering_drive.on_for_seconds(direction, SpeedPercent(-speed), TIME_STEP * max_steps, False, False)
+        self.steering_drive.on_for_seconds(direction, SpeedPercent(-speed), LineFollowerTest.TIME_STEP * max_steps, False, False)
         while (self.steering_drive.is_running or self.steering_drive.is_ramping) and not on_track() and self.color_sensor.color != 6:
-            #self.steering_drive.on_for_seconds(direction, SpeedPercent(-speed), TIME_STEP, False)
+            #self.steering_drive.on_for_seconds(direction, SpeedPercent(-speed), LineFollowerTest.TIME_STEP, False)
             i = i + 1
             #if i >= max_steps:
             #    break
@@ -72,18 +69,18 @@ class LineFollowerTest:
 
     def direction_name(self, turn_direction):
         if turn_direction < 0:
-            return 'left'
+            return 'LineFollowerTest.LEFT'
         else:
             return 'right'
 
     def turn_and_seek(self, max_turn_steps, turn_direction):
         name = direction_name(turn_direction)
         print('Turn ' + name)
-        result = move_until_on_track(100 * turn_direction, SEARCH_SPEED, max_turn_steps)
+        result = move_until_on_track(100 * turn_direction, LineFollowerTest.SEARCH_SPEED, max_turn_steps)
         if result == -1:
             return True
         print('Turn back')
-        self.steering_drive.on_for_seconds(100 * turn_direction * -1, SpeedPercent(-SEARCH_SPEED), TIME_STEP * result, False)
+        self.steering_drive.on_for_seconds(100 * turn_direction * -1, SpeedPercent(-LineFollowerTest.SEARCH_SPEED), LineFollowerTest.TIME_STEP * result, False)
         return False
 
     def find_line(self, max_turn_steps):
@@ -106,7 +103,7 @@ class LineFollowerTest:
 
     def run(self):
         direction = 1
-        max_turn_steps = SEARCH_MAX_STEPS
+        max_turn_steps = LineFollowerTest.SEARCH_MAX_STEPS
 
         while True:
             if direction == 1:
@@ -114,19 +111,19 @@ class LineFollowerTest:
             else:
                 print('Backward')
 
-            move_while_on_track(0, FORWARD_SPEED * direction)
+            move_while_on_track(0, LineFollowerTest.FORWARD_SPEED * direction)
 
             # Go back after seeing yellow (track-specific)
             if self.color_sensor.color == COLOR_YELLOW:
-                self.steering_drive.on_for_seconds(0, SpeedPercent(-FORWARD_SPEED), TIME_STEP * 8, False)
-                self.steering_drive.on_for_degrees(-100, SpeedPercent(SEARCH_SPEED), 90, False)
+                self.steering_drive.on_for_seconds(0, SpeedPercent(-LineFollowerTest.FORWARD_SPEED), LineFollowerTest.TIME_STEP * 8, False)
+                self.steering_drive.on_for_degrees(-100, SpeedPercent(LineFollowerTest.SEARCH_SPEED), 90, False)
 
             if self.color_sensor.color == 5:
                 break
 
             found = False
             i = 0
-            max_turn_steps = SEARCH_MAX_STEPS
+            max_turn_steps = LineFollowerTest.SEARCH_MAX_STEPS
             while i < 5:
                 if find_line(max_turn_steps):
                     found = True
