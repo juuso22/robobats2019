0845a846953bba1c470fadca46fd400b7ee79802
Ola L <ola.lansman@relexsolutions.com>
2019-10-12 14:47:08 +0300

    round obstacle

--- round_obstacle.py
+++ round_obstacle.py
@@ -6,76 +6,63 @@ from ev3dev2.wheel import EV3Tire
 import time
 import code
 
-steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
-# Params: Andle (-100 to 100), speed (percent, 0 to 100), seconds it drives.
+class RoundObstacle:
+    
+    steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+    # Params: Andle (-100 to 100), speed (percent, 0 to 100), seconds it drives.
 
-LEFT = 1
-RIGHT = -1
+    LEFT = 1
+    RIGHT = -1
 
-color_sensor = ColorSensor(address=INPUT_1)
-infrared_sensor = InfraredSensor(INPUT_2)
+    color_sensor = ColorSensor(address=INPUT_1)
+    infrared_sensor = InfraredSensor(INPUT_2)
 
-def drive_until_proximity(proximity, direction):
-    steering_drive.on(direction, SpeedPercent(-100))
-    while (infrared_sensor.proximity > proximity):
-        time.sleep(0.2)
-        print('Infrared sensor is ' + str(infrared_sensor.proximity))
-    steering_drive.off()
-
-
-
-#steering_drive.on_for_seconds(0, SpeedPercent(-100), 2)
-def wait_and_drive(direction, proximity, maxholes = 0, sleep_time = 0.3): 
-    last_proximity = 0
-    i = 0
-    while True:
-        curr_prox = infrared_sensor.proximity
-        if (curr_prox < proximity):
-            last_proximity = curr_prox
+    def drive_until_proximity(proximity, direction):
+        steering_drive.on(direction, SpeedPercent(-100))
+        while (infrared_sensor.proximity > proximity):
             time.sleep(0.2)
-            print('Infrared sensor is ' + str(curr_prox))
-            continue
-        elif(last_proximity >= proximity):
-            last_proximity = curr_prox
-            time.sleep(0.1)
-            print('Infrared sensor is ' + str(curr_prox))
-            continue
-        elif(i < maxholes):
-            last_proximity = curr_prox
-            i += 1
-            print('Found first aukko')
-            continue
-        else:
-            break
-
-    time.sleep(sleep_time)
-    print('CHARGE')
-    drive_until_proximity(40, direction)
-
-
-def test():
-    drive_until_proximity(40, 0)
-    wait_and_drive(8 * RIGHT, 75, maxholes = 1, sleep_time = 0.5)
-    drive_until_proximity(40, 0)
-    wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
-    wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
-
-code.interact(local = locals())
+            print('Infrared sensor is ' + str(infrared_sensor.proximity))
+        steering_drive.off()
 
 
 
+    #steering_drive.on_for_seconds(0, SpeedPercent(-100), 2)
+    def wait_and_drive(direction, proximity, maxholes = 0, sleep_time = 0.3): 
+        last_proximity = 0
+        i = 0
+        while True:
+            curr_prox = infrared_sensor.proximity
+            if (curr_prox < proximity):
+                last_proximity = curr_prox
+                time.sleep(0.2)
+                print('Infrared sensor is ' + str(curr_prox))
+                continue
+            elif(last_proximity >= proximity):
+                last_proximity = curr_prox
+                time.sleep(0.1)
+                print('Infrared sensor is ' + str(curr_prox))
+                continue
+            elif(i < maxholes):
+                last_proximity = curr_prox
+                i += 1
+                print('Found first aukko')
+                continue
+            else:
+                break
 
+        time.sleep(sleep_time)
+        print('CHARGE')
+        drive_until_proximity(40, direction)
 
-# go straight
+    def test():
+        drive_until_proximity(40, 0)
+        wait_and_drive(8 * self.RIGHT, 75, maxholes = 1, sleep_time = 0.5)
+        drive_until_proximity(40, 0)
+        wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
+        wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
 
-# turn left
+def main():
+    code.interact(local = locals())
 
-# go straight
-
-# turn right
-
-# Move to conter
-
-# Face to the right direction
-
-
+if __name__ == "__main__":
+    main()
