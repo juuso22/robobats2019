2b5ccb8655439fa6bdf4adea3a5a5e11255e236f
Ola L <ola.lansman@relexsolutions.com>
2019-10-12 15:18:59 +0300

    round obstacle

--- round_obstacle.py
+++ round_obstacle.py
@@ -7,9 +7,7 @@ import time
 import code
 
 class RoundObstacle:
-    
     steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
-    # Params: Andle (-100 to 100), speed (percent, 0 to 100), seconds it drives.
 
     LEFT = 1
     RIGHT = -1
@@ -17,49 +15,60 @@ class RoundObstacle:
     color_sensor = ColorSensor(address=INPUT_1)
     infrared_sensor = InfraredSensor(INPUT_2)
 
-    def drive_until_proximity(proximity, direction):
-        steering_drive.on(direction, SpeedPercent(-100))
-        while (infrared_sensor.proximity > proximity):
+    def drive_until_proximity(self, proximity_goal = 40, direction = 0):
+        if (self.infrared_sensor.proximity > proximity_goal):
+            self.steering_drive.on(direction, SpeedPercent(-100))
+        while (self.infrared_sensor.proximity > proximity_goal):
             time.sleep(0.2)
-            print('Infrared sensor is ' + str(infrared_sensor.proximity))
-        steering_drive.off()
+            print('Dist: ' + str(self.infrared_sensor.proximity))
+        self.steering_drive.off()
 
-
-
-    #steering_drive.on_for_seconds(0, SpeedPercent(-100), 2)
-    def wait_and_drive(direction, proximity, maxholes = 0, sleep_time = 0.3): 
+    def wait_and_sleep(self, proximity = 70, maxholes = 0, sleep_time = 0.3): 
         last_proximity = 0
         i = 0
         while True:
-            curr_prox = infrared_sensor.proximity
+            curr_prox = self.infrared_sensor.proximity
             if (curr_prox < proximity):
                 last_proximity = curr_prox
                 time.sleep(0.2)
-                print('Infrared sensor is ' + str(curr_prox))
+                print('Dist: ' + str(curr_prox))
                 continue
             elif(last_proximity >= proximity):
                 last_proximity = curr_prox
                 time.sleep(0.1)
-                print('Infrared sensor is ' + str(curr_prox))
+                print('Dist: ' + str(curr_prox))
                 continue
             elif(i < maxholes):
                 last_proximity = curr_prox
                 i += 1
-                print('Found first aukko')
+                print('Aukko number:' + str(i))
                 continue
             else:
                 break
 
         time.sleep(sleep_time)
-        print('CHARGE')
-        drive_until_proximity(40, direction)
 
-    def test():
-        drive_until_proximity(40, 0)
-        wait_and_drive(8 * self.RIGHT, 75, maxholes = 1, sleep_time = 0.5)
-        drive_until_proximity(40, 0)
-        wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
-        wait_and_drive(0, 75, maxholes = 1, sleep_time = 0.5)
+    def first_kiekko(self):
+        self.drive_until_proximity()
+        self.wait_and_sleep(75, maxholes = 1, sleep_time = 0.5)
+        self.drive_until_proximity(direction = 8 * self.RIGHT)
+
+    def second_kiekko_left(self):
+        self.drive_until_proximity()
+        self.wait_and_sleep(75, maxholes = 1, sleep_time = 0.5)
+        self.drive_until_proximity(direction = 8 * self.RIGHT)
+
+
+    def third_kiekko(self):
+        self.drive_until_proximity()
+        self.wait_and_sleep(75, maxholes = 1, sleep_time = 1)
+        self.drive_until_proximity()
+
+    def start(self):
+        self.first_kiekko()
+        self.second_kiekko_left()
+        self.third_kiekko()
+
 
 def main():
     code.interact(local = locals())
