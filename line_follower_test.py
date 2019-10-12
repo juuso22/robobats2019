127cd17d46bd5a432b6321b12b426914162d9ede
Juuso Terävä <juuso.terava@relex.fi>
2019-10-12 14:35:05 +0300

    Calibrate white in line follower testing + update gitignore

--- line_follower_test.py
+++ line_follower_test.py
@@ -136,6 +136,7 @@ class LineFollowerTest:
 def main():
     movesteering = MoveSteering(OUTPUT_A, OUTPUT_B)
     colorsensor = ColorSensor(address=INPUT_1)
+    colosensor.calibrate_white()
     follower = LineFollowerTest(movesteering, colorsensor)
     follower.run()
 
