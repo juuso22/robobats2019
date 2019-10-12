5060e9bb465311677b6b2c98f88de9954197adf3
Ola L <ola.lansman@relexsolutions.com>
2019-10-12 22:16:56 +0300

    changed forest

--- forest.py
+++ forest.py
@@ -7,16 +7,14 @@ import time
 import code
 
 class Forest:
-    def woop(self):
-        TIME_MULTIPLIER = 1
-        SPEED_MULTIPLIER = 1
+    def woop(self, TIME_MULTIPLIER = 1, DIRECTION_MULTIPLIEER = 1):
         steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
-        steering_drive.on_for_seconds(-30 * SPEED_MULTIPLIER,  -100 * SPEED_MULTIPLIER,  3 * TIME_MULTIPLIER)
-        steering_drive.on_for_seconds(35 * SPEED_MULTIPLIER, -100 * SPEED_MULTIPLIER,  4.1 * TIME_MULTIPLIER)
-        steering_drive.on_for_seconds(0 * SPEED_MULTIPLIER,  -100 * SPEED_MULTIPLIER,  1.8 * TIME_MULTIPLIER)
-        steering_drive.on_for_seconds(-40 * SPEED_MULTIPLIER,  -100 * SPEED_MULTIPLIER,  1.3 * TIME_MULTIPLIER)
-        steering_drive.on_for_seconds(0 * SPEED_MULTIPLIER,  -100 * SPEED_MULTIPLIER,  0.8 * TIME_MULTIPLIER)
-        steering_drive.on_for_seconds(40 * SPEED_MULTIPLIER,  -100 * SPEED_MULTIPLIER,  1 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(-0 * DIRECTION_MULTIPLIEER,  100,  3 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(-35 * DIRECTION_MULTIPLIEER, 100,  4.1 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(0 * DIRECTION_MULTIPLIEER,  100,  1.8 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(40 * DIRECTION_MULTIPLIEER,  1100,  1.3 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(0 * DIRECTION_MULTIPLIEER,  1100,  0.8 * TIME_MULTIPLIER)
+        steering_drive.on_for_seconds(-40 * DIRECTION_MULTIPLIEER,  1100,  1 * TIME_MULTIPLIER)
 
 def main():
     forest = Forest()
